5c33b389eabafa043f5608c84a3d9c94374f000e
Luuk Hafkamp <luuk@Luuks-MacBook-Pro-2.local>
2018-03-03 18:03:58 +0100

    First setup

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const path = require('path');
+const TelegramBot = require('node-telegram-bot-api');
+const Stomp = require('@stomp/stompjs');
+require('dotenv').config();
+const app = express();
+
+// connect to Spectrals RabbitMQ to allow for a stomp socket connection
+const client = Stomp.client(process.env.SPECTRAL_DB_URL);
+client.connect(process.env.MQTT_USER, process.env.MQTT_PASS, onConnect, console.error, '/');
+
+// connection endpoints
+const endpoints = {
+	'#': '/exchange/power/#',  // all
+	'00': '/exchange/power/00', // main meter
+	'A0': '/exchange/power/A0', // metabolic lab
+	'A1': '/exchange/power/A1', // tech boat
+	'B0': '/exchange/power/B0', // crossboat
+	'C0': '/exchange/power/C0', // cafe
+	'01': '/exchange/power/01', // boats
+	'02': '/exchange/power/02',
+	'03': '/exchange/power/03',
+	'04': '/exchange/power/04',
+	'05': '/exchange/power/05',
+	'06': '/exchange/power/06',
+	'07': '/exchange/power/07',
+	'08': '/exchange/power/08',
+	'09': '/exchange/power/09',
+	'10': '/exchange/power/10',
+	'11': '/exchange/power/11',
+	'12': '/exchange/power/12',
+	'13': '/exchange/power/13',
+	'14': '/exchange/power/14',
+	'Biogasboat': '/exchange/biogasboat', // biogasboat
+	'Liveliness': '/exchange/liveliness' // liveliness
+};
+
+// on connection with RabbitMQ, subscribe to a boats data
+function onConnect() {
+	client.subscribe(endpoints.Liveliness, onData);
+}
+
+// checks which probes are on/off (return true/false)
+function onData(data) {
+	const probes = JSON.parse(data.body);
+	const keys = Object.keys(probes);
+	const trueProbes = keys.filter(key => probes[key]);
+	const falseProbes = keys.filter(key => !probes[key]);
+
+	subscribeToTrue(trueProbes)
+}
+
+function subscribeToTrue(probes) {
+	// probes.forEach(probe => {
+	// 	client.subscribe(endpoints[probe], test);
+	// });
+
+
+	//** TEST **//
+	const testProbe = probes[0];
+	console.log(`testing... ${testProbe}`);
+	client.subscribe(endpoints[probes[testProbe]], checkForEmptyObject);
+}
+
+function checkForEmptyObject(data) {
+	console.log(data.body);
+
+	// if object is empty
+}
+
+// bot stuff
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {  
+	var greeting = 'Hi';
+	var response = 'Hello Human';
+	if (msg.text === greetings) {
+		bot.sendMessage(msg.chat.id, response);
+	}
+});
+
+// get the public files
+app.use(express.static('public'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// render the index page
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.listen(9001, () => {
+	console.log('server running on 9001..');
+});
